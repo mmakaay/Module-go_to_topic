de5911f93f33085e98fd5662d10491ed26b0cc78
Maurice Makaay <maurice@phorum.org>
2011-07-29 07:36:27 +0200

    Import of Phorum 5.1 version of the Go To Topic module code.

--- db.php
+++ db.php
@@ -2,6 +2,53 @@
 
 if(!defined("PHORUM")) return;
 
+function mod_go_to_topic_db_query($sql)
+{
+    $type = $GLOBALS["PHORUM"]["DBCONFIG"]["type"];
+
+    switch ($type)
+    {
+      case "mysql":
+        $conn = phorum_db_mysql_connect();
+        $res = mysql_query($sql, $conn);
+        if ($res && mysql_num_rows($res)) {
+          $res = mysql_fetch_array($res);
+        } else {
+          $res = NULL;
+        }
+        break;
+
+      case "mysqli":
+        $conn = phorum_db_mysqli_connect();
+        $res = mysqli_query($conn, $sql);
+        if ($res && mysqli_num_rows($res)) {
+          $res = mysqli_fetch_array($res);
+        } else {
+          $res = NULL;
+        }
+        break;
+
+      case postgresql:
+        $conn = phorum_db_postgresql_connect();
+        $res = pg_query($conn, $sql);
+        if ($res && pg_num_rows($res)) {
+          $res = pg_fetch_row($res);
+        } else {
+          $res = NULL;
+        }
+        break;
+
+      default:
+        trigger_error(
+            "The Go To Topic module currently contains no database " .
+            "implementation for database type \"".htmlspecialchars($type)."\"",
+            E_USER_ERROR
+        );
+    }
+
+    return $res;
+}
+
 function mod_go_to_topic_get_thread_offset($message_id)
 {
     $PHORUM = $GLOBALS["PHORUM"];
@@ -10,20 +57,12 @@ function mod_go_to_topic_get_thread_offset($message_id)
 
     // Find the thread id for the message_id. We need to look at the
     // threads to find the page on which a message_id can be found.
-    $row = phorum_db_interact(
-        DB_RETURN_ROW,
-        "SELECT thread, modifystamp, sort
+    $row = mod_go_to_topic_db_query(
+        "SELECT thread, modifystamp
          FROM   {$PHORUM['message_table']}
          WHERE  message_id = $message_id"
     );
     if ($row === NULL) return NULL; // Message not found.
-
-    // When this is a sticky message, then it will always be shown
-    // on page 1.
-    if ($row[2] == PHORUM_SORT_STICKY) {
-      return 0;
-    }
-
     $thread_id   = $row[0];
     $modifystamp = $row[1];
 
@@ -37,8 +76,7 @@ function mod_go_to_topic_get_thread_offset($message_id)
         // modifystamp above.
         if (!$modifystamp || $message_id != $thread_id)
         {
-            $row = phorum_db_interact(
-                DB_RETURN_ROW,
+            $row = mod_go_to_topic_db_query(
                 "SELECT modifystamp
                  FROM   {$PHORUM['message_table']}
                  WHERE  message_id = $thread_id"
@@ -46,18 +84,13 @@ function mod_go_to_topic_get_thread_offset($message_id)
             $modifystamp = $row === NULL ? 0 : $row[0];
         }
 
-        $condition =
-            "(" .
-            "modifystamp > $modifystamp OR " .
-            "(modifystamp = $modifystamp AND message_id > $thread_id)" .
-            ")";
+        $condition = "modifystamp > $modifystamp";
     } else {
         $condition = "message_id > $thread_id";
     }
 
     // Now, find the offset of the message in the forum.
-    $offset = phorum_db_interact(
-        DB_RETURN_VALUE,
+    $row = mod_go_to_topic_db_query(
         "SELECT count(*)
          FROM   {$PHORUM['message_table']}
          WHERE  $condition AND
@@ -65,22 +98,9 @@ function mod_go_to_topic_get_thread_offset($message_id)
                 status      = ".PHORUM_STATUS_APPROVED." AND
                 parent_id   = 0"
     );
+    $offset = $row[0];
 
-    // Compensate for the sticky threads.
-    // This is done in a separate query, so the counting can make use of
-    // an index. When combined with the query from above, the performance
-    // was lacking.
-    $skip_special_threads = phorum_db_interact(
-        DB_RETURN_VALUE,
-        "SELECT count(*)
-         FROM   {$PHORUM['message_table']}
-         WHERE  forum_id    = {$PHORUM['forum_id']} AND
-                status      = ".PHORUM_STATUS_APPROVED." AND
-                sort        = ".PHORUM_SORT_STICKY." AND
-                parent_id   = 0"
-    );
-
-    return ($offset - $skip_special_threads);
+    return $offset;
 }
 
 ?>
--- go_to_topic.php
+++ go_to_topic.php
@@ -7,100 +7,39 @@ if (! defined("PHORUM")) return;
 // Load the database layer code.
 require_once("./mods/go_to_topic/db.php");
 
-// Apply default settings. When changing this code, then remember to update
-// the defaults in settings.php as well.
-if (!isset($PHORUM['mod_go_to_topic'])) {
-    $PHORUM['mod_go_to_topic'] = array( 
-        'replace_original_list_urls' => TRUE
-    );  
-}   
-
-function phorum_mod_go_to_topic_page_list()
+function phorum_mod_go_to_topic_common()
 {
     global $PHORUM;
 
-    // Catch requests that carry a gototopic argument.
-    if (isset($PHORUM['args']['gototopic'])) {
-        phorum_mod_go_to_topic_redirect($PHORUM['args']['gototopic']);
+    // Catch requests for the list page, which have a gototopic argument set.
+    if (phorum_page == "list" && isset($PHORUM["args"]["gototopic"])) {
+        phorum_mod_go_to_topic_redirect($PHORUM["args"]["gototopic"]);
+    }
+
+    // On the read page, format the gototopic URL.
+    elseif (phorum_page == "read" && isset($PHORUM["args"][1]))
+    {
+        // Point back to a message id if we have one and if
+        // we're running in threaded list mode. Else point back
+        // to the thread id.
+        $id = (isset($PHORUM["args"][2]) && $PHORUM["threaded_list"]) 
+            ? $PHORUM["args"][2] : $PHORUM["args"][1];
+
+        $url = phorum_get_url(
+            PHORUM_LIST_URL, 
+            $PHORUM["forum_id"], 
+            "gototopic=$id"
+        );
+        $PHORUM["DATA"]["GO_TO_TOPIC_URL"] = $url;
     }
 }
 
-function phorum_mod_go_to_topic_start_output()
+function phorum_mod_go_to_topic_post_post($msg)
 {
-    global $PHORUM;
+    $PHORUM = $GLOBALS["PHORUM"];
 
-    // Find the forum, thread and message id for the message that we
-    // want to jump to on the list page.
-    $forum_id   = $PHORUM['forum_id'];
-    $thread_id  = NULL;
-    $message_id = NULL;
-
-    // On the read page, we can extract the data from the arguments.
-    if (phorum_page === 'read')
-    {
-        // Only do the Go To Topic URL formatting if we have the
-        // necessary data available in the URL.
-        if (!empty($PHORUM['args'][1]))
-        {
-            $thread_id  = $PHORUM['args'][1];
-            $message_id = empty($PHORUM['args'][2])
-                        ? $thread_id : $PHORUM['args'][2];
-        }
-    }
-    // On other pages, we might have the message available in the
-    // ref_thread_id and ref_message_id parameters.
-    else
-    {
-        if (!empty($PHORUM['args']['ref_thread_id'])) {
-            $thread_id = $PHORUM['args']['ref_thread_id'];
-            $message_id = empty($PHORUM['args']['ref_message_id'])
-                        ? $thread_id : $PHORUM['args']['ref_message_id'];
-        }
-    }
-
-    // Thread or message id found? Then generate the Go To Topic URL.
-    if ($thread_id !== NULL)
-    {
-        // Point back to a message id if we are running in threaded list
-        // mode. Else point back to the thread id.
-        $id = $PHORUM['threaded_list'] ? $message_id : $thread_id;
-
-        // Generate the URL.
-        $url = phorum_get_url(PHORUM_LIST_URL, $forum_id, "gototopic=$id");
-
-        // Make the URL available to template authors.
-        $PHORUM['DATA']['GO_TO_TOPIC_URL'] = $url;
-
-        // If the module is configured to automatically replace the
-        // list URLs in the page, then handle that now.
-        if (!empty($PHORUM['mod_go_to_topic']['replace_original_list_urls']))
-        {
-            // Update the plain list URL.
-            $PHORUM['DATA']['URL']['LIST'] = $url;
-
-            // Fix the breadcrumbs.
-            foreach ($PHORUM['DATA']['BREADCRUMBS'] as $id => $node) {
-               if ($node['TYPE'] == 'forum' && $node['ID'] == $forum_id) {
-                   $PHORUM['DATA']['BREADCRUMBS'][$id]['URL'] = $url;
-                   break; // there should be only one
-               }
-            }
-        }
-    }
-}
-
-function phorum_mod_go_to_topic_after_post($msg)
-{
-    global $PHORUM;
-
-    // If the user is redirected to the read page after posting, then we
-    // don't need to change the redirect behavior.
-    if ($PHORUM["redirect_after_post"] == "read") {
-        return $msg;
-    }
-
-    // If a reply to a standard (non-sticky) thread is posted, the we do a
-    // redirect to the correct list page for the thread.
+    // If a reply to a standard thread is posted, the we do a redirect
+    // to the message or thread on the list page.
     if ($msg["parent_id"] != 0 && $msg["sort"] == PHORUM_SORT_DEFAULT)
     {
         // Redirect to the thread or the message? If the forum is in
@@ -121,9 +60,9 @@ function phorum_mod_go_to_topic_redirect($message_id)
     settype($message_id, "int");
 
     // Determine the number of messages that goes on a list page.
-    $list_length = $PHORUM['threaded_list']
-                 ? $PHORUM['list_length_threaded']
-                 : $PHORUM['list_length_flat'];
+    $list_length = $PHORUM['threaded_list'] 
+                 ? $PHORUM['list_length_threaded'] 
+                 : $PHORUM['list_length_flat']; 
 
     // Find the thread offset for the message in the forum.
     $offset = mod_go_to_topic_get_thread_offset($message_id);
@@ -148,5 +87,7 @@ function phorum_mod_go_to_topic_redirect($message_id)
         PHORUM_REDIRECT_URL,
         'phorum_redirect_to=' . urlencode($url)
     ));
+
+    exit();
 }
 ?>
